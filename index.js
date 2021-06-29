3c5d38b6656c9561b027a711a43cb7d181ae2b10
Fabrizio <a20160348@pucp.edu.pe>
2021-06-29 13:11:46 -0500

    P4

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require('mysql2');
 var app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(8080,function (){
+app.listen(8080, function () {
     console.log("Servidor levantado correctamente");
 });
 
 var connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"lab10_employees",
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "lab10_employees",
     port: "3306"
 });
 
 connection.connect(function (error) {
-    if(error){
+    if (error) {
         console.log(error);
-    }else{
+    } else {
         console.log("Conexion correcta a BD");
     }
 });
 
 // P1
-app.get("/empleados/get",function (request, response) {
+app.get("/empleados/get", function (request, response) {
     var query = "select e.EmployeeID, e.LastName, e.FirstName, e.Title from employees e";
-    connection.query(query,function (error,result) {
-        if(error){
+    connection.query(query, function (error, result) {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
 
-            for (let i=0; i<result.length; i++){
-                result[i].orden=i+1;
+            for (let i = 0; i < result.length; i++) {
+                result[i].orden = i + 1;
             }
 
             response.json(result);
@@ -43,27 +43,65 @@ app.get("/empleados/get",function (request, response) {
 });
 
 //P2
-app.get("/empleados/getManagerEmployees/:id",function (request,response) {
+app.get("/empleados/getManagerEmployees/:id", function (request, response) {
     var id = request.params.id;
     var query = "select e.EmployeeID, e.LastName, e.FirstName, e.Title from employees e inner join employees j on (e.ReportsTo=j.EmployeeID) where j.EmployeeID = ?";
-    connection.query(query,[id],function (error,result) {
-        if(error){
+    connection.query(query, [id], function (error, result) {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             response.json(result);
         }
     });
 });
 
 //P3
-app.get("/empleados/get/:id",function (request,response) {
+app.get("/empleados/get/:id", function (request, response) {
     var id = request.params.id;
     var query = " select * from employees e where e.Title like ?";
-    connection.query(query,[id],function (error,result) {
-        if(error){
+    connection.query(query, [id], function (error, result) {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             response.json(result);
         }
     });
+});
+
+//P4
+app.post("/empleados/update", function (request, response) {
+    let id = request.body.employeeid;
+    let email = request.body.email;
+    let address = request.body.address;
+    var query1 = "update employees e set e.Email = ? where e.EmployeeID = ?";
+    var query2 = "update employees e set e.Address = ? where e.EmployeeID= ?";
+    var query3 = "update employees e set e.Email = ? and e.Address where EmployeeID= ?";
+    if (email !== '' && address !== ''){
+        let parametros = [email, address, id];
+        connection.query(query3, parametros, function (error, result) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.json(result);
+            }
+        });
+    }else if(email !== '' && address === ''){
+        let parametros = [email, id];
+        connection.query(query1, parametros, function (error, result) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.json(result);
+            }
+        });
+    }else if(email === '' && address !== ''){
+        let parametros = [address, id];
+        connection.query(query2, parametros, function (error, result) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.json(result);
+            }
+        });
+    }
 });
